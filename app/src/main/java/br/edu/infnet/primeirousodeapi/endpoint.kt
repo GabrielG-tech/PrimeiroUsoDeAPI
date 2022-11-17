e31ea80dcf1405b26d02ad3236ab1be4b2629ab0
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-11-17 11:50:52 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/infnet/primeirousodeapi/endpoint.kt
@@ -0,0 +1,10 @@
+package br.edu.infnet.primeirousodeapi
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface Endpoint {
+    @GET("posts")
+    fun getPosts() : Call<List<Posts>>
+
+}
---
+++ app/src/main/java/br/edu/infnet/primeirousodeapi/model.kt
@@ -0,0 +1,14 @@
+package br.edu.infnet.primeirousodeapi
+
+import com.google.gson.annotations.SerializedName
+
+data class Posts(
+    @SerializedName("userId")
+    var userId : Int,
+    @SerializedName("id")
+    var id : Int,
+    @SerializedName("title")
+    var title : String,
+    @SerializedName("body")
+    var body : String
+)
